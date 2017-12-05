5178f49c1ce4e166b5ee61ae73901fae86672928
Tim Sheils <timothy.sheils@nih.gov>
2017-12-05 14:33:14 -0500

    add dist

---
+++ src/app/services/node-expand.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class NodeExpandService {
+  private  expandMap: Map<string, Expand> = new Map();
+
+  fetchExpand(node: string):Expand{
+    return this.expandMap.get(node) || new Expand();
+  }
+/*
+  let ex = this.expandMap.get(node);
+  if(ex){
+    return ex;
+}else{
+  return new Expand();
+}*/
+
+  setExpand(node:string, expand: Expand):void{
+    this.expandMap.set(node, expand);
+  }
+
+  constructor() { }
+
+}
+
+export class Expand {
+  all:boolean = false;
+  compound: boolean = false;
+  pattern: boolean = false;
+  predictions: boolean = false;
+  target: boolean = false;
+
+  constructor() {}
+}
