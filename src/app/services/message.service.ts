efcec02910f032484f2c2ae348fa349fe2880a90
Brett Yang <brettyang@Nextonics-MacBook-Pro.local>
2023-08-25 07:24:07 -0400

    Update {qParam} to $qParam.

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -13,13 +13,13 @@ export class MessageService {
 
       case 'chembl':
       case 'target': {
-        msg = 'MATCH (n:Target) WHERE n.uniprot_id= {qParam} MATCH (n)-[r:REGULATES]-(b) RETURN n, r, b';
+        msg = 'MATCH (n:Target) WHERE n.uniprot_id= $qParam MATCH (n)-[r:REGULATES]-(b) RETURN n, r, b';
         params = {qParam: term};
         break;
       }
 
       case 'compound': {
-        msg = 'MATCH (n:Compound) WHERE n.compound= {qParam} MATCH (n)-[r]-(b) RETURN n, r, b LIMIT 5';
+        msg = 'MATCH (n:Compound) WHERE n.compound= $qParam MATCH (n)-[r]-(b) RETURN n, r, b LIMIT 5';
         params = {qParam: term};
         break;
       }
@@ -27,15 +27,15 @@ export class MessageService {
       case 'counts': {
         switch (properties) {
           case 'Target': {
-            msg = 'MATCH (n:Target) WHERE n.uuid = {qParam}  MATCH (n)-[r]-(b) RETURN DISTINCT labels(b),COUNT(labels(b))';
+            msg = 'MATCH (n:Target) WHERE n.uuid = $qParam  MATCH (n)-[r]-(b) RETURN DISTINCT labels(b),COUNT(labels(b))';
             break;
           }
           case 'Compound': {
-            msg = 'MATCH (n:Compound) WHERE n.uuid = {qParam}  MATCH (n)-[r]-(b) RETURN DISTINCT labels(b),COUNT(labels(b))';
+            msg = 'MATCH (n:Compound) WHERE n.uuid = $qParam  MATCH (n)-[r]-(b) RETURN DISTINCT labels(b),COUNT(labels(b))';
             break;
           }
           case 'Pattern': {
-            msg = 'MATCH (n:Pattern) WHERE n.uuid = {qParam}  MATCH (n)-[r]-(b) RETURN DISTINCT labels(b),COUNT(labels(b))';
+            msg = 'MATCH (n:Pattern) WHERE n.uuid = $qParam  MATCH (n)-[r]-(b) RETURN DISTINCT labels(b),COUNT(labels(b))';
             break;
           }
         }
@@ -46,8 +46,8 @@ export class MessageService {
       case 'endNodeSearch':
       case 'startNodeSearch': {
         // todo: convern nostereo_hash to a contains in hash search
-        msg = 'MATCH (n:Target) WHERE n.uniprot_id IN {qParam} RETURN n AS data UNION MATCH (c:Compound) WHERE c.nostereo_hash IN {qParam} RETURN c AS data';
-        //   msg = 'MATCH (n:Target) WHERE n.uniprot_id IN {qParam} RETURN n UNION MATCH (n:Compound) WHERE n.hash IN {qParam} RETURN n';
+        msg = 'MATCH (n:Target) WHERE n.uniprot_id IN $qParam RETURN n AS data UNION MATCH (c:Compound) WHERE c.nostereo_hash IN $qParam RETURN c AS data';
+        //   msg = 'MATCH (n:Target) WHERE n.uniprot_id IN $qParam RETURN n UNION MATCH (n:Compound) WHERE n.hash IN $qParam RETURN n';
         params = {qParam: term};
         break;
       }
@@ -56,20 +56,20 @@ export class MessageService {
         const start: string = 'MATCH (n:' + properties.origin;
         switch (properties.target) {
           case 'Target': {
-            //  msg = 'MATCH p=shortestPath((t)-[r*..1]->(q:Target)) WHERE t.uuid = {qParam} return p LIMIT 100';
-            msg = start + '{uuid:{qParam}}) MATCH (n)-[r]-(b:Target) with {segments:[{start: startNode(r), relationship:r, end: endNode(r)}]} AS ret RETURN ret LIMIT 100';
+            //  msg = 'MATCH p=shortestPath((t)-[r*..1]->(q:Target)) WHERE t.uuid = $qParam return p LIMIT 100';
+            msg = start + '{uuid:$qParam}) MATCH (n)-[r]-(b:Target) with {segments:[{start: startNode(r), relationship:r, end: endNode(r)}]} AS ret RETURN ret LIMIT 100';
             break;
           }
           case 'Compound': {
-            msg = start + '{uuid:{qParam}}) MATCH (n)-[r]-(b:Compound) with {segments:[{start: startNode(r), relationship:r, end: endNode(r)}]} AS ret RETURN ret LIMIT 100';
+            msg = start + '{uuid:$qParam}) MATCH (n)-[r]-(b:Compound) with {segments:[{start: startNode(r), relationship:r, end: endNode(r)}]} AS ret RETURN ret LIMIT 100';
             break;
           }
           case 'Pattern': {
-            msg = start + '{uuid:{qParam}}) MATCH (n)-[r]-(b:Pattern) with {segments:[{start: startNode(r), relationship:r, end: endNode(r)}]} AS ret RETURN ret LIMIT 100';
+            msg = start + '{uuid:$qParam}) MATCH (n)-[r]-(b:Pattern) with {segments:[{start: startNode(r), relationship:r, end: endNode(r)}]} AS ret RETURN ret LIMIT 100';
             break;
           }
           case 'All': {
-            msg = 'MATCH (n) WHERE n.uuid = {qParam} MATCH (n)-[r]-(b) with {segments:[{start: startNode(r), relationship:r, end: endNode(r)}]} AS ret RETURN ret LIMIT 100';
+            msg = 'MATCH (n) WHERE n.uuid = $qParam MATCH (n)-[r]-(b) with {segments:[{start: startNode(r), relationship:r, end: endNode(r)}]} AS ret RETURN ret LIMIT 100';
             break;
           }
         }
@@ -78,7 +78,7 @@ export class MessageService {
       }
 
       case 'node': {
-        msg = 'MATCH (n:Target) WHERE n.uniprot_id= {qParam} RETURN n';
+        msg = 'MATCH (n:Target) WHERE n.uniprot_id= $qParam RETURN n';
         params = {qParam: term};
         break;
       }
@@ -189,21 +189,21 @@ export class MessageService {
       }
 
       case 'prediction': {
-        // msg = 'MATCH (t:Target) WHERE t.uuid= {qParam} MATCH (t)<-[r1:POTENT_PATTERN_OF]-(p:Pattern) MATCH (p)-[r2:PATTERN_OF]->(c:Compound) WHERE NOT ((c)-[:TESTED_ON]->(t)) RETURN t, r1, p, r2, c LIMIT 300';
-        msg = 'MATCH (t:Target) WHERE t.uuid= {qParam} MATCH (t)<-[r1:POTENT_PATTERN_OF]-(p:Pattern) MATCH (p)-[r2:PATTERN_OF]->(c:Compound) WHERE NOT ((c)-[:TESTED_ON]->(t))' +
+        // msg = 'MATCH (t:Target) WHERE t.uuid= $qParam MATCH (t)<-[r1:POTENT_PATTERN_OF]-(p:Pattern) MATCH (p)-[r2:PATTERN_OF]->(c:Compound) WHERE NOT ((c)-[:TESTED_ON]->(t)) RETURN t, r1, p, r2, c LIMIT 300';
+        msg = 'MATCH (t:Target) WHERE t.uuid= $qParam MATCH (t)<-[r1:POTENT_PATTERN_OF]-(p:Pattern) MATCH (p)-[r2:PATTERN_OF]->(c:Compound) WHERE NOT ((c)-[:TESTED_ON]->(t))' +
           'with {segments:[{start: startNode(r1), relationship:r1, end: endNode(r1)},{start: startNode(r2), relationship:r2, end: endNode(r2)}]} AS ret RETURN ret LIMIT 300';
         params = {qParam: term};
         break;
       }
 
       case 'smiles': {
-        msg = 'MATCH (n:Pattern) WHERE n.pid= {qParam} MATCH (n)-[r]-(b) RETURN n, r, b LIMIT 5';
+        msg = 'MATCH (n:Pattern) WHERE n.pid= $qParam MATCH (n)-[r]-(b) RETURN n, r, b LIMIT 5';
         params = {qParam: term};
         break;
       }
 
       case 'uuid': {
-        msg = 'MATCH (n) WHERE n.uuid= {qParam} RETURN n';
+        msg = 'MATCH (n) WHERE n.uuid= $qParam RETURN n';
         params = {qParam: term};
         break;
       }
